5eddc98e65e8ddc7f4809ae7c9a6afa39c8a4ea7
Samuel Coco <Samuel.HenriAC@outlook.com>
2023-06-16 00:38:17 +0100

    credentials crud WIP 16-06-23 00:38:00 WIP

--- screens/VaultScreen.jsx
+++ screens/VaultScreen.jsx
@@ -7,94 +7,99 @@ import AppSearchBar from '../components/AppSearchBar';
 
 import * as SQLite from 'expo-sqlite';
 
-const db = SQLite.openDatabase("passvault.db");
-
+const db = SQLite.openDatabase('passvault.db');
 
 function VaultScreen(props) {
-    const [web, setWeb] = useState([]);
-    const [card, setCard] = useState([]);
-    const [credentialProviders, setCredentialProviders] = useState([]);
+  const [web, setWeb] = useState([]);
+  const [card, setCard] = useState([]);
+  const [credentialProviders, setCredentialProviders] = useState([]);
 
-    useEffect(() => {
-      db.transaction((tx) => {
-        tx.executeSql(
-          'SELECT id, name FROM web',
-          [],
-          (_, { rows }) => {
-            const webRecords = rows._array;
-            setWeb(webRecords);
-            console.log("Returned Web Records: " + webRecords)
-            updateCredentialProviders();
-          },
-          (error) => {
-            console.log('Error retrieving web records:', error);
-          }
-        );
-  
-        tx.executeSql(
-          'SELECT id, name FROM card',
-          [],
-          (_, { rows }) => {
-            const cardRecords = rows._array;
-            setCard(cardRecords);
-            updateCredentialProviders();
-          },
-          (error) => {
-            console.log('Error retrieving card records:', error);
-          }
-        );
-      });
-    }, []);
+  const fetchRecords = () => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        'SELECT id, name FROM web',
+        [],
+        (_, { rows }) => {
+          const webRecords = rows._array;
+          setWeb(webRecords);
+          console.log('Returned Web Records:', webRecords);
+          updateCredentialProviders();
+        },
+        (error) => {
+          console.log('Error retrieving web records:', error);
+        }
+      );
 
-    const updateCredentialProviders = () => {
-      const providers = [
-        ...web.map((record) => ({
-          id: record.id,
-          name: record.name,
-          image: require('../assets/icon.png'),
-        })),
-        ...card.map((record) => ({
-          id: record.id,
-          name: record.name,
-          image: require('../assets/icon.png'),
-        })),
-      ];
-      setCredentialProviders(providers);
-    };
+      tx.executeSql(
+        'SELECT id, name FROM card',
+        [],
+        (_, { rows }) => {
+          const cardRecords = rows._array;
+          setCard(cardRecords);
+          updateCredentialProviders();
+        },
+        (error) => {
+          console.log('Error retrieving card records:', error);
+        }
+      );
+    });
+  };
 
-    return (
-           <Screen>
-              
-                <View style={ {flexDirection: "row", alignItems: "center", backgroundColor: "white"}}>
-                <AppCredentialMetric
-                  image={require('../assets/icon.png')}
-                  text="Web"
-                  subText="5"
-                />
-                <AppCredentialMetric
-                  image={require('../assets/icon.png')}
-                  text="Card"
-                  subText="3"
-                />
-                <AppCredentialMetric
-                  image={require('../assets/icon.png')}
-                  text="Cust."
-                  subText="1"
-                />
-                </View>
-              
-                <AppSearchBar />
+  useEffect(() => {
+    fetchRecords();
+  }, []);
 
-                <FlatList 
-                  data={credentialProviders}
-                  renderItem={ ({ item }) => (
-                      <AppCredentialProvider provider={item} />
-                  )}
-                  // keyExtractor={(item) => item.id}
-                />
-           </Screen>
-        
-    );
+  const updateCredentialProviders = () => {
+    const providers = [
+      ...web.map((record) => ({
+        id: record.id,
+        name: record.name,
+        image: require('../assets/icon.png'),
+      })),
+      ...card.map((record) => ({
+        id: record.id,
+        name: record.name,
+        image: require('../assets/icon.png'),
+      })),
+    ];
+    setCredentialProviders(providers);
+  };
+
+  return (
+    <Screen>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          backgroundColor: 'white',
+        }}
+      >
+        <AppCredentialMetric
+          image={require('../assets/icon.png')}
+          text="Web"
+          subText="5"
+        />
+        <AppCredentialMetric
+          image={require('../assets/icon.png')}
+          text="Card"
+          subText="3"
+        />
+        <AppCredentialMetric
+          image={require('../assets/icon.png')}
+          text="Cust."
+          subText="1"
+        />
+      </View>
+
+      <AppSearchBar />
+
+      <FlatList
+        data={credentialProviders}
+        renderItem={({ item }) => <AppCredentialProvider provider={item} />}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </Screen>
+  );
 }
 
 export default VaultScreen;
