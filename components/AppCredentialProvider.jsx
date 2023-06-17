91873bbb445156da546649ba6ed63a487b328a44
Samuel Coco <Samuel.HenriAC@outlook.com>
2023-06-17 20:10:38 +0100

    Credentials-crud WIP

--- components/AppCredentialProvider.jsx
+++ components/AppCredentialProvider.jsx
@@ -1,25 +1,69 @@
 import React, { useState } from 'react';
-
 import { View, Image, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { SwipeListView }  from 'react-native-swipe-list-view';
+import AppWebCredential from './AppWebCredential';
+
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabase('passvault.db');
 
 const AppCredentialProvider = ({ provider }) => {
-    const [id, setId] = useState(provider.id);
-    const [type, setType] = useState(provider.type);
-    const [credentials, setCredential] = useState([]);
+  const [id, setId] = useState(provider.id);
+  const [type, setType] = useState(provider.type);
+  const [credentials, setCredentials] = useState([]);
+  const [showList, setShowList] = useState(false);
 
-    return (
-        <TouchableOpacity> 
-            <View style={styles.container}>
-                <Image source={provider.image} style={styles.image} />
-                <Text style={styles.label}>{provider.name}</Text>
-            </View>
-        </TouchableOpacity>
-    );
-    };
+  const fetchData = () => {
+    db.transaction((tx) => {
+        if (type === 'web') {
+            tx.executeSql(
+                'SELECT id, username, password FROM web_credential WHERE id = ?',
+                [id],
+                (_, { rows }) => {
+                  const webCredentialRecords = rows._array;
+                  setCredentials(webCredentialRecords);
+                  setShowList(true);
+                  console.log('FOUND WEB_CREDENTIALS: ', webCredentialRecords);
+                },
+                (error) => {
+                  console.log('Error retrieving web credential records:', error);
+                }
+              );
+        }
+    });
+  };
 
+  const closeList = () => {
+    setShowList(false);
+  };
+
+  const renderItem = ({ item }) => {
+    const { username, password } = item;
+    return <AppWebCredential username={username} password={password} />;
+  };
+
+  return (
+    <View>
+      <TouchableOpacity onPress={fetchData}>
+        <View style={styles.container}>
+          <Image source={provider.image} style={styles.image} />
+          <Text style={styles.label}>{provider.name}</Text>
+        </View>
+      </TouchableOpacity>
+      {showList && (
+        <SwipeListView
+          data={credentials}
+          renderItem={renderItem}
+          onRowDidOpen={closeList}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      )}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-container: {
+  container: {
     flexDirection: 'row',
     alignItems: 'center',
     borderWidth: 1,
@@ -27,16 +71,17 @@ container: {
     borderRadius: 15,
     padding: 10,
     marginVertical: 6,
-    marginHorizontal: 4
-},
-image: {
+    marginHorizontal: 4,
+  },
+  image: {
     width: 50,
     height: 50,
     marginRight: 10,
-},
-label: {
+  },
+  label: {
     fontSize: 12,
-    color: "black"
-},
+    color: 'black',
+  },
 });
+
 export default AppCredentialProvider;
