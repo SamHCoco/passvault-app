c47b44a79f08601b21374911b35d26fc8b6e0885
Samuel Coco <Samuel.HenriAC@outlook.com>
2023-06-17 19:36:03 +0100

    Credentials crud WIP

--- components/AppCredentialProvider.jsx
+++ components/AppCredentialProvider.jsx
@@ -1,38 +1,42 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 
-import { View, Image, Text, StyleSheet } from 'react-native';
+import { View, Image, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const AppCredentialProvider = ({ provider }) => {
     const [id, setId] = useState(provider.id);
     const [type, setType] = useState(provider.type);
+    const [credentials, setCredential] = useState([]);
 
     return (
-        <View style={styles.container}>
-        <Image source={provider.image} style={styles.image} />
-        <Text style={styles.label}>{provider.name}</Text>
-        </View>
+        <TouchableOpacity> 
+            <View style={styles.container}>
+                <Image source={provider.image} style={styles.image} />
+                <Text style={styles.label}>{provider.name}</Text>
+            </View>
+        </TouchableOpacity>
     );
     };
 
-    const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        borderWidth: 1,
-        borderColor: 'black',
-        borderRadius: 15,
-        padding: 10,
-        marginVertical: 6,
-        marginHorizontal: 4
-    },
-    image: {
-        width: 50,
-        height: 50,
-        marginRight: 10,
-    },
-    label: {
-        fontSize: 12,
-        color: "black"
-    },
-    });
+
+const styles = StyleSheet.create({
+container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 15,
+    padding: 10,
+    marginVertical: 6,
+    marginHorizontal: 4
+},
+image: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+},
+label: {
+    fontSize: 12,
+    color: "black"
+},
+});
 export default AppCredentialProvider;
