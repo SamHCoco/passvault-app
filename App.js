32cfee72cf8cd09ead064e8cd27d3d2d28dc84a7
Samuel Coco <Samuel.HenriAC@outlook.com>
2023-06-18 01:39:18 +0100

    wip

--- App.js
+++ App.js
@@ -1,26 +1,23 @@
 import React, { useEffect, useState } from 'react';
-
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
 import { View, TextInput, StyleSheet } from 'react-native';
-
 import * as SQLite from 'expo-sqlite';
-
 import VaultScreen from './screens/VaultScreen';
 import BackupScreen from './screens/BackupScreen';
 import SettingsScreen from './screens/SettingsScreen';
-
+import EditWebCredentialScreen from './screens/EditWebCredentialScreen';
 import AppIcon from './components/AppIcon';
 
 const Tab = createBottomTabNavigator();
-
 const db = SQLite.openDatabase({ name: 'passvault.db', location: 'default' });
+const Stack = createStackNavigator();
 
 export default function App() {
   const [webCredentialCount, setWebCredentialCount] = useState(0);
   const [cardCredentialCount, setCardCredentialCount] = useState(0);
-
 
   useEffect(() => {
     const createTables = () => {
@@ -76,37 +73,47 @@ export default function App() {
 
   return (
     <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-
-              if (route.name === 'Vault') {
-                iconName = focused ? 'home' : 'home-outline';
-              } else if (route.name === 'Backup') {
-                iconName = focused ? 'cloud-upload' : 'cloud-upload-outline';
-              } else if (route.name === 'Settings') {
-                iconName = focused ? 'cog' : 'cog-outline';
-              }
-
-              return (
-                <AppIcon
-                  name={iconName}
-                  size={size}
-                  color={color}
-                />
-              );
-            }
-          })}
-          tabBarOptions={{
-            activeTintColor: 'dodgerblue',
-            inactiveTintColor: 'grey'
-          }}
-          >
-          <Tab.Screen name="Vault" component={VaultScreen} />
-          <Tab.Screen name="Backup" component={BackupScreen} />
-          <Tab.Screen name="Settings" component={SettingsScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen name="Tabs" component={TabScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="EditWebCredentialScreen" component={EditWebCredentialScreen} />
+      </Stack.Navigator>
+      
+    </NavigationContainer>
   );
+
+  function TabScreen() {
+      return (
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Vault') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Backup') {
+              iconName = focused ? 'cloud-upload' : 'cloud-upload-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'cog' : 'cog-outline';
+            }
+
+            return (
+              <AppIcon
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
+          }
+        })}
+        tabBarOptions={{
+          activeTintColor: 'dodgerblue',
+          inactiveTintColor: 'grey'
+        }}
+      >
+        <Tab.Screen name="Vault" component={VaultScreen} />
+        <Tab.Screen name="Backup" component={BackupScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+      );
+  }
 }
