329eb68c660940d32b937dae7f0c1298bdf32135
Samuel Coco <Samuel.HenriAC@outlook.com>
2023-06-19 20:44:45 +0100

    wip

--- screens/EditWebCredentialScreen.jsx
+++ screens/EditWebCredentialScreen.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, ScrollView } from 'react-native';
 import { Formik, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
 
 import Screen from '../components/Screen';
 import AppTextInput from '../components/AppTextInput';
@@ -9,44 +8,12 @@ import AppRoundTouchable from '../components/AppRoundTouchable';
 import AppPasswordGenerator from '../components/AppPasswordGenerator';
 import AppToggleButton from '../components/AppToggleButton';
 
+import { validationSchema } from '../service/validationSchemas';
+
 import saveWebCredential from '../service/saveWebCredential';
 import saveCardCredential from '../service/saveCardCredential';
 import AppSlider from '../components/AppSlider';
-
-const validationSchema = Yup.object().shape({
-  url: Yup.string().when('selectedOption', {
-    is: 'Web',
-    then: Yup.string().url('Invalid URL').required('URL is required'),
-  }),
-  username: Yup.string().when('selectedOption', {
-    is: 'Web',
-    then: Yup.string().required('Username is required'),
-  }),
-  password: Yup.string().when('selectedOption', {
-    is: 'Web',
-    then: Yup.string().required('Password is required'),
-  }),
-  bank: Yup.string().when('selectedOption', {
-    is: 'Card',
-    then: Yup.string().required('Bank is required'),
-  }),
-  cardNumber: Yup.string().when('selectedOption', {
-    is: 'Card',
-    then: Yup.string().matches(/^\d{16}$/, 'Invalid card number').required('Card number is required'),
-  }),
-  expirationMonth: Yup.string().when('selectedOption', {
-    is: 'Card',
-    then: Yup.string().matches(/^(0[1-9]|1[0-2])$/, 'Invalid expiration month').required('Expiration month is required'),
-  }),
-  expirationYear: Yup.string().when('selectedOption', {
-    is: 'Card',
-    then: Yup.string().matches(/^\d{2}$/, 'Invalid expiration year').required('Expiration year is required'),
-  }),
-  securityCode: Yup.string().when('selectedOption', {
-    is: 'Card',
-    then: Yup.string().max(4, 'Security code must be at most 4 characters'),
-  }),
-});
+import AppIcon from '../components/AppIcon';
 
 function EditWebCredentialScreen(props) {
   const [selectedOption, setSelectedOption] = useState('Web');
@@ -67,74 +34,82 @@ function EditWebCredentialScreen(props) {
     if (selectedOption === 'Web') {
       return (
         <>
-          <Field
-            component={AppTextInput}
-            name="url"
-            placeholder="URL"
-            autoCapitalize="none"
-            autoCorrect={false}
-          />
-          <ErrorMessage name="url" component={Text} style={styles.errorText} />
+          <View style={styles.webFormContainer}>
+            <Field
+              component={AppTextInput}
+              name="url"
+              placeholder="URL"
+              autoCapitalize="none"
+              autoCorrect={false}
+            />
+            <ErrorMessage name="url" component={Text} style={styles.errorText} />
 
-          <Field
-            component={AppTextInput}
-            name="username"
-            placeholder="Username"
-            autoCapitalize="none"
-            autoCorrect={false}
-          />
-          <ErrorMessage name="username" component={Text} style={styles.errorText} />
+            <View style={{ flexDirection: 'row', alignContent: 'center', justifyContent: 'center', borderWidth: 1}}>
+                <AppIcon name="md-person-circle" color="black" size={45} library="ion" />
+            </View>
 
-          <Field
-            component={AppTextInput}
-            name="password"
-            placeholder="Password"
-            autoCapitalize="none"
-            autoCorrect={false}
-            secureTextEntry
-          />
-          <ErrorMessage name="password" component={Text} style={styles.errorText} />
+            <Field
+              component={AppTextInput}
+              name="username"
+              placeholder="Username"
+              autoCapitalize="none"
+              autoCorrect={false}
+            />
+            <ErrorMessage name="username" component={Text} style={styles.errorText} />
 
-          <View>
-            <AppSlider value={sliderValue} label="Characters" onValueChange={(value) => setSliderValue(value)}  />
-          <AppToggleButton label="Special characters" onToggle={(value) => console.log("Set value enabled")} />
-          <AppToggleButton label="Numbers" onToggle={(value) => console.log("Set value enabled")} />
-          <AppToggleButton label="Uppercase" onToggle={(value) => console.log("Set value enabled")} />
-          <AppToggleButton label="Lowercase" onToggle={(value) => console.log("Set value enabled")} />
-          <AppRoundTouchable label="Generate" />
+            <Field
+              component={AppTextInput}
+              name="password"
+              placeholder="Password"
+              autoCapitalize="none"
+              autoCorrect={false}
+              secureTextEntry
+            />
+            <ErrorMessage name="password" component={Text} style={styles.errorText} />
+
+            <View style={{borderWidth: 0}}>
+            <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+                <AppRoundTouchable text="Save" onPress={handleSubmit} />
+                <AppRoundTouchable text="Generate" />
+            </View>
+              <AppSlider value={sliderValue} label="Characters" onValueChange={(value) => setSliderValue(value)} />
+              <AppToggleButton label="Special characters" onToggle={(value) => console.log("Set value enabled")} />
+              <AppToggleButton label="Numbers" onToggle={(value) => console.log("Set value enabled")} />
+              <AppToggleButton label="Uppercase" onToggle={(value) => console.log("Set value enabled")} />
+              <AppToggleButton label="Lowercase" onToggle={(value) => console.log("Set value enabled")} />
+            </View>
           </View>
         </>
       );
     } else if (selectedOption === 'Card') {
       return (
         <>
-          <Screen>
-              <View style={styles.bankContainer}>
-                <Text style={styles.bankLabel}>Bank</Text>
-                <Field
-                  component={AppTextInput}
-                  name="bank"
-                  placeholder="Bank"
-                  autoCapitalize="none"
-                  autoCorrect={false}
-                />
-                <ErrorMessage name="bank" component={Text} style={styles.errorText} />
-              </View>
-              
-              <View style={styles.cardContainer}>
-                <Text style={styles.cardLabel}>Card Number</Text>
-                <Field
-                  component={AppTextInput}
-                  name="cardNumber"
-                  placeholder="Card Number"
-                  keyboardType="numeric"
-                  style={styles.cardInput}
-                  maxLength={16}
-                />
-                <ErrorMessage name="cardNumber" component={Text} style={styles.errorText} />
-              </View>
+          <View style={styles.bankFormContainer}>
+              <Text style={styles.bankLabel}>Bank</Text>
+              <Field
+                component={AppTextInput}
+                name="bank"
+                placeholder="Bank"
+                autoCapitalize="none"
+                autoCorrect={false}
+              />
+              <ErrorMessage name="bank" component={Text} style={styles.errorText} />
+            </View>
 
-              <View style={styles.expirationContainer}>
+            <View style={styles.cardContainer}>
+              <Text style={styles.cardLabel}>Card Number</Text>
+              <Field
+                component={AppTextInput}
+                name="cardNumber"
+                placeholder="Card Number"
+                keyboardType="numeric"
+                style={styles.cardInput}
+                maxLength={16}
+              />
+              <ErrorMessage name="cardNumber" component={Text} style={styles.errorText} />
+            </View>
+
+            <View style={styles.expirationContainer}>
               <View style={styles.expirationMonthContainer}>
                 <Text style={styles.expirationLabel}>Exp Month</Text>
                 <Field
@@ -161,20 +136,21 @@ function EditWebCredentialScreen(props) {
             </View>
 
             <View style={styles.securityCodeContainer}>
-                <Text style={styles.securityCodeLabel}>Security Code</Text>
-                <Field
-                  component={AppTextInput}
-                  name="securityCode"
-                  placeholder="Security Code"
-                  autoCapitalize="none"
-                  autoCorrect={false}
-                  secureTextEntry
-                  style={styles.securityCodeInput}
-                  maxLength={4}
-                />
-                <ErrorMessage name="securityCode" component={Text} style={styles.errorText} />
-              </View>
-          </Screen>
+              <Text style={styles.securityCodeLabel}>Security Code</Text>
+              <Field
+                component={AppTextInput}
+                name="securityCode"
+                placeholder="Security Code"
+                autoCapitalize="none"
+                autoCorrect={false}
+                secureTextEntry
+                style={styles.securityCodeInput}
+                maxLength={4}
+              />
+              <ErrorMessage name="securityCode" component={Text} style={styles.errorText} />
+
+              <AppRoundTouchable text="Save" onPress={handleSubmit} />
+            </View>
         </>
       );
     }
@@ -183,58 +159,74 @@ function EditWebCredentialScreen(props) {
   };
 
   return (
-    <Screen>
-      <View style={styles.container}>
-        <View style={styles.optionsContainer}>
-          <AppRoundTouchable
-            iconName="web"
-            iconColor='black'
-            iconLibrary='material'
-            iconSize={65}
-            isSelected={selectedOption === 'Web'}
-            onPress={() => setSelectedOption('Web')}
-          />
-          <AppRoundTouchable
-            name="card"
-            iconName='card'
-            iconColor='black'
-            iconLibrary='ion'
-            iconSize={65}
-            isSelected={selectedOption === 'Card'}
-            onPress={() => setSelectedOption('Card')}
-          />
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <Screen>
+        <View style={styles.container}>
+          <View style={styles.optionsContainer}>
+            <AppRoundTouchable
+              iconName="web"
+              iconColor="black"
+              iconLibrary="material"
+              iconSize={65}
+              isSelected={selectedOption === 'Web'}
+              onPress={() => setSelectedOption('Web')}
+            />
+            <AppRoundTouchable
+              name="card"
+              iconName="card"
+              iconColor="black"
+              iconLibrary="ion"
+              iconSize={65}
+              isSelected={selectedOption === 'Card'}
+              onPress={() => setSelectedOption('Card')}
+            />
+          </View>
+
+          <Formik
+            initialValues={{
+              url: '',
+              username: '',
+              password: '',
+              bank: '',
+              cardNumber: '',
+              expirationMonth: '',
+              expirationYear: '',
+              securityCode: '',
+            }}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ handleChange, handleSubmit, errors, touched }) => (
+              <>
+                {renderForm()}
+
+              </>
+            )}
+          </Formik>
         </View>
-
-        <Formik
-          initialValues={{
-            url: '',
-            username: '',
-            password: '',
-            bank: '',
-            cardNumber: '',
-            expirationMonth: '',
-            expirationYear: '',
-            securityCode: '',
-          }}
-          validationSchema={selectedOption === 'Web' ? validationSchema.web : validationSchema.card}
-          onSubmit={handleSubmit}
-        >
-          {({ handleChange, handleSubmit, errors, touched }) => (
-            <>
-              {renderForm()}
-
-              <AppRoundTouchable text="Save" onPress={handleSubmit} />
-            </>
-          )}
-        </Formik>
-      </View>
-    </Screen>
+      </Screen>
+    </ScrollView>
   );
 }
 
 const styles = {
+  webFormContainer: {
+    borderWidth: 1,
+    borderRadius: 12,
+    width: 365
+  },
+  bankFormContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginBottom: 4,
+    borderRadius: 12
+  },
+  scrollContainer: {
+    flexGrow: 1,
+  },
   container: {
     alignItems: 'center',
+    paddingBottom: 20,
   },
   optionsContainer: {
     flexDirection: 'row',
@@ -285,11 +277,6 @@ const styles = {
   //   flex: 1,
   //   alignItems: 'center'
   // },
-  bankContainer: {
-    flexDirection: 'column',
-    alignItems: 'center',
-    marginBottom: 4,
-  },
   bankLabel: {
     marginRight: 10,
   },
