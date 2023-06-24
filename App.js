2b9dcf4aab7e1c43316e1eb6de56558abb8ac655
Samuel Coco <Samuel.HenriAC@outlook.com>
2023-06-24 19:31:36 +0100

    responsive layout based on device added

--- App.js
+++ App.js
@@ -15,58 +15,57 @@ import createTables from './service/createTable';
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
+function TabScreen() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Vault') {
+            iconName = focused ? 'home' : 'home-outline';
+          // } else if (route.name === 'Backup') {
+          //   iconName = focused ? 'cloud-upload' : 'cloud-upload-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'cog' : 'cog-outline';
+          }
+
+          return (
+            <AppIcon
+              name={iconName}
+              size={size}
+              color={color}
+            />
+          );
+        }
+      })}
+      tabBarOptions={{
+        activeTintColor: 'dodgerblue',
+        inactiveTintColor: 'grey'
+      }}
+    >
+      <Tab.Screen name="Vault" component={VaultScreen} />
+      {/* <Tab.Screen name="Backup" component={BackupScreen} /> */}
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
 export default function App() {
   const [webCredentialCount, setWebCredentialCount] = useState(0);
   const [cardCredentialCount, setCardCredentialCount] = useState(0);
 
   useEffect(() => {
-    createTables(); 
+    createTables();
   }, []);
 
   return (
     <NavigationContainer>
-        <Stack.Navigator>
-        <Stack.Screen name="Tabs" component={TabScreen} options={{ headerShown: false }}/>
+      <Stack.Navigator initialRouteName="Vault">
+        <Stack.Screen name="Tabs" component={TabScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Credential" component={EditWebCredentialScreen} />
         <Stack.Screen name="Vault" component={VaultScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-
-  function TabScreen() {
-      return (
-        <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Vault') {
-              iconName = focused ? 'home' : 'home-outline';
-            // } else if (route.name === 'Backup') {
-            //   iconName = focused ? 'cloud-upload' : 'cloud-upload-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'cog' : 'cog-outline';
-            }
-
-            return (
-              <AppIcon
-                name={iconName}
-                size={size}
-                color={color}
-              />
-            );
-          }
-        })}
-        tabBarOptions={{
-          activeTintColor: 'dodgerblue',
-          inactiveTintColor: 'grey'
-        }}
-      >
-        <Tab.Screen name="Vault" component={VaultScreen} />
-        {/* <Tab.Screen name="Backup" component={BackupScreen} /> */}
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-        
-      </Tab.Navigator>
-      );
-  }
 }
--- components/Screen.jsx
+++ components/Screen.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import Constants from 'expo-constants';
-import { SafeAreaView, StyleSheet, View, ScrollView, Platform, StatusBar } from 'react-native';
+import { SafeAreaView, StyleSheet, View, ScrollView, Platform, StatusBar, Dimensions } from 'react-native';
+
+const windowHeight = Dimensions.get('window').height;
 
 function Screen(props) {
-    return (
-        <SafeAreaView style={styles.screen}>
-            {/* <ScrollView>{props.children} */}
-            <View style={styles.view}>{props.children}</View>
-            {/* </ScrollView> */}
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.screen}>
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View style={styles.view}>{props.children}</View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
 
-// rnss - shortcut
 const styles = StyleSheet.create({
-    screen: {
-        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
-    },
-    view: {
-        backgroundColor: "white"
-    }
-})
+  screen: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+    paddingBottom: windowHeight * 0.02,
+  },
+  view: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingHorizontal: windowWidth * 0.05,
+  },
+});
 
 export default Screen;
