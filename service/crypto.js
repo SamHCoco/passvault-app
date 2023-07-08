2f95da87f9768c2da0d317b7859ff820f29cb90b
Samuel Coco <Samuel.HenriAC@outlook.com>
2023-07-08 16:36:12 +0100

    wip

--- service/crypto.js
+++ service/crypto.js
@@ -2,6 +2,8 @@ import { AES, enc } from 'react-native-crypto-js';
 import * as Crypto from 'expo-crypto';
 import { encode, decode } from './base64';
 
+var CryptoJS = require('crypto-js')
+
 const ENCRYPTION_KEY_SIZE = 32; // 256-bit key for AES
 const ENCRYPTION_IV_SIZE = 16; // 128-bit IV for AES
 
@@ -23,7 +25,8 @@ export async function encryptValue(plainTextValue, key) {
   );
 
   const encryptionIV = await Crypto.getRandomBytesAsync(ENCRYPTION_IV_SIZE);
-  const encryptionIVBase64 = bytesToBase64(encryptionIV);
+  const encryptionIVBase64 = bytesToBase64(encryptionIV).slice(0, ENCRYPTION_IV_SIZE);
+  console.log("CREATED IV: ", encryptionIVBase64); // todo - remove
 
   const encryptedValue = AES.encrypt(
     plainTextValue,
@@ -39,21 +42,25 @@ export async function encryptValue(plainTextValue, key) {
 
 // Decrypts the encrypted value using AES symmetric key decryption
 export async function decryptValue(combinedValue, key) {
-  const encryptionIVBase64 = combinedValue.slice(0, ENCRYPTION_IV_SIZE * 4);
-  const encryptedValue = combinedValue.slice(ENCRYPTION_IV_SIZE * 4);
+  const encryptionIVBase64 = combinedValue.slice(0, ENCRYPTION_IV_SIZE);
+  console.log("DECRYPT step - IV: ", encryptionIVBase64); // todo - remove
+  const encryptedValue = combinedValue.slice(ENCRYPTION_IV_SIZE);
+  console.log("DECRYPT step - EncryptedValue: ", encryptedValue); // todo - remove
 
   const decryptionKey = await Crypto.digestStringAsync(
     Crypto.CryptoDigestAlgorithm.SHA256,
     key
   );
 
-  const decryptionIV = base64ToBytes(encryptionIVBase64);
-
-  const decryptedValue = AES.decrypt(
+  const decryptedValue = CryptoJS.AES.decrypt(
     encryptedValue,
     decryptionKey.slice(0, ENCRYPTION_KEY_SIZE), // Use only the first 32 bytes (256 bits) of the key
-    { iv: decryptionIV }
-  ).toString(enc.Utf8); // Convert the decrypted value to UTF-8 string
+    { iv: encryptionIVBase64 }
+  ); // Convert the decrypted value to UTF-8 string
 
-  return decryptedValue;
+  const decryptedValueString = decryptedValue.toString(enc.Utf8); // Convert the decrypted value to UTF-8 string
+
+  console.log("DECRYPTED VALUE: ", decryptedValueString); // todo - remove
+
+  return decryptedValueString;
 }
