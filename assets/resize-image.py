c2d62dae2b845d853cf85a021b360b200e51e15c
Samuel Coco <Samuel.HenriAC@outlook.com>
2023-07-25 21:11:16 +0100

    Icon fixed

---
+++ assets/resize-image.py
@@ -0,0 +1,18 @@
+from PIL import Image
+
+def resize_image(input_image_path, output_image_path, width, height):
+    try:
+        with Image.open(input_image_path) as img:
+            resized_img = img.resize((width, height), Image.LANCZOS)
+            resized_img.save(output_image_path)
+            print("Image successfully resized.")
+    except IOError:
+        print("Unable to load the image.")
+
+if __name__ == "__main__":
+    input_image_path = "./passvault-icon-final.png"
+    output_image_path = "./passvault-icon-final-square.png"
+    target_width = 485  # Specify your desired width here
+    target_height = 485  # Specify your desired height here
+
+    resize_image(input_image_path, output_image_path, target_width, target_height)
